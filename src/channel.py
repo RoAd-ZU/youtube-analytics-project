f99c6667723ef853251ef7ef0504b948bd491a9d
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-08-29 23:52:37 +0300

    channel.py HW2

--- src/channel.py
+++ src/channel.py
@@ -5,18 +5,46 @@ import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
+
+
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.youtube = Channel.get_service()
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.channel_id = channel_id
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = 'https://www.youtube.com/channel/' + channel_id
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
-api_key: str = os.getenv('API_KEY')
 
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+
+    def to_json(self, jsonfile):
+        data = [{'id': self.channel_id,
+                'title': self.title,
+                'description': self.description,
+                'url': self.url,
+                'subscriber_count': self.subscriber_count,
+                'video_count': self.video_count,
+                'view_count': self.view_count}]
+        with open(jsonfile, 'w', ) as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
+
+api_key: str = os.getenv('API_KEY')
 """Здесь оставил некоторые проверки для себя"""
 # chan_youtube = Channel(api_key)
 # chan_youtube.print_info()
